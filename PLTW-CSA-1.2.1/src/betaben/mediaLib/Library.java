2d252e40265e309b4530d6843bbdf16528c1ab69
BetaBen1 <bbhardin1@gmail.com>
2016-09-13 09:17:17 -0700

    PLTW-CSA-1.2.1

--- PLTW-CSA-1.2.1/src/betaben/mediaLib/Library.java
+++ PLTW-CSA-1.2.1/src/betaben/mediaLib/Library.java
@@ -14,30 +14,6 @@ public class Library {
 		books = new ArrayList<Book>();
 	}
 	
-	public void loadFromFile(String file){
-		String text[] = NebulousIO.getFileAsString(file).split(",");
-		for(int i = 0; i < text.length; i += 4){
-			if(text[i].equals("S")){
-				songs.add(new Song(text[i + 1], Double.parseDouble(text[i + 2]), Double.parseDouble(text[i + 3])));
-				System.out.println("Added Song: " + text[i + 1] + ", " + text[i + 2] + ", " + text[i + 3]);
-			} else if(text[i].equals("B")){
-				books.add(new Book(text[i + 1], Double.parseDouble(text[i + 2]), Double.parseDouble(text[i + 3])));
-				System.out.println("Added Book: " + text[i + 1] + ", " + text[i + 2] + ", " + text[i + 3]);
-			} else if(text[i].equals("M")){
-				movies.add(new Movie(text[i + 1], Double.parseDouble(text[i + 2]), Double.parseDouble(text[i + 3])));
-				System.out.println("Added Movie: " + text[i + 1] + ", " + text[i + 2] + ", " + text[i + 3]);
-			} else { System.out.println("ERROR: Could not read line from file"); }
-		} System.out.println("\n");
- 	}
-	
-	public void saveToFile(String file){
-		String totalString = "";
-		for(Song song : songs) totalString += "S" + "," + song.getName() + "," + song.getRating() + "," + song.getPrice() + "," + "\n";
-		for(Book book : books) totalString += "B" + "," + book.getName() + "," + book.getRating() + "," + book.getPrice() + "," + "\n";
-		for(Movie movie : movies) totalString += "M" + "," + movie.getName() + "," + movie.getRating() + "," + movie.getPrice() + "," + "\n";
-		NebulousIO.saveStringToFile(totalString, file);
-	}
-	
 	public void addMovie(Movie movie){
 		movies.add(movie);
 	}
