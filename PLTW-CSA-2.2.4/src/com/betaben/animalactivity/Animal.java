5de12fd2a47e12b82440769e15bfd990ef9f3d29
BenHardin <bbhardin1@gmail.com>
2016-11-17 12:24:13 -0500

    Updates
    New work on PLTW-CSA-2.2.4.

---
+++ PLTW-CSA-2.2.4/src/com/betaben/animalactivity/Animal.java
@@ -0,0 +1,7 @@
+package com.betaben.animalactivity;
+
+public abstract class Animal {
+
+	public abstract String say();
+	
+}
---
+++ PLTW-CSA-2.2.4/src/com/betaben/animalactivity/Duck.java
@@ -0,0 +1,10 @@
+package com.betaben.animalactivity;
+
+public class Duck extends Animal {
+
+	@Override
+	public String say() {
+		return("quack");
+	}
+	
+}
